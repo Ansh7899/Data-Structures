fd3589ef980f98c98b1c172884b3eaf809ad6370
Ansh Kumar Gupta <63894658+Ansh7899@users.noreply.github.com>
2021-06-16 17:17:51 +0530

    added tree traversal

--- Trees/BT.cpp
+++ Trees/BT.cpp
@@ -22,9 +22,45 @@ struct node *create()
     newnode->right = create();
     return newnode;
 }
+void preorder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << "\t";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << "\t";
+    inorder(root->right);
+}
+void postorder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << "\t";
+}
 int main()
 {
     struct node *root;
     root = NULL;
     root = create();
+    cout << "\nPreOrder Traversal of the tree is: ";
+    preorder(root);
+    cout << "\nInOrder Traversal of the tree is: ";
+    inorder(root);
+    cout << "\nPostOrder Traversal of the tree is: ";
+    postorder(root);
 }
