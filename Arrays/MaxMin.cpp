b269df62b65a66f03c3fa6c2edaa695056668e96
Ansh Kumar Gupta <63894658+Ansh7899@users.noreply.github.com>
2021-06-18 19:44:40 +0530

    completed max-min.cpp

---
+++ Arrays/MaxMin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 312, 2, 234, 24, 134, 46, 346, 23};
+    int min = INT_MAX, max = INT_MIN;
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    cout << "\nmax element: " << max << "\nmin element: " << min;
+    return 0;
+}
