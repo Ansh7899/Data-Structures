a924b4df0b398cb317ae768c32cc04b4b1128e15
Ansh Kumar Gupta <63894658+Ansh7899@users.noreply.github.com>
2021-06-20 20:10:34 +0530

    added merging of arrays

--- Arrays/MergeSortedArrays.cpp
+++ Arrays/MergeSortedArrays.cpp
@@ -4,4 +4,22 @@ int main()
 {
     int a[] = {1, 14, 18, 19, 43, 90};
     int b[] = {0, 25, 29, 31, 90, 78, 101};
+
+    int na = sizeof(a) / sizeof(a[0]);
+    int nb = sizeof(b) / sizeof(b[0]);
+    int c[na + nb];
+    int i = 0, k = 0, j = 0;
+    while (i < na && j < nb)
+    {
+        if (a[i] < b[j])
+            c[k++] = a[i++];
+        else
+            c[k++] = b[j++];
+    }
+    while (i < na)
+        c[k++] = a[i++];
+    while (j < nb)
+        c[k++] = b[j++];
+    for (int i = 0; i < na + nb; i++)
+        cout << c[i] << "\t";
 }
